3678263b6f034f817ef991fb72e65aa506867dcd
ssr0016 <samsonrecluta0016@gmail.com>
2023-10-14 20:55:15 +0800

    adding some features in my typescipt todo

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,34 +1,36 @@
-import React, { useRef } from 'react'
-import "./styles.css"
+import React, { useRef } from "react";
+import "./styles.css";
 
-interface Props {
-    todo: string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
-    handleAdd: (e: React.FormEvent)=> void; // void if this function is not going to return anything
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField:React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="input" onSubmit={(e)=>{
-      handleAdd(e)
-      inputRef.current?.blur()
-      }}>
-        <input
-         ref={inputRef}
-         value={todo}
-         onChange={
-            (e)=>setTodo(e.target.value)
-         }
-         type="input"
-         placeholder="Enter a task"
-         className="input_box" />
-        <button className="input_submit" type="submit">
-        Go
-        </button>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input_box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete} from "react-icons/ai"
 import { MdDone } from "react-icons/md";
@@ -9,27 +9,78 @@ type Props = {
   todo:Todo,
   todos:Todo[],
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-
 }
 
+
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+  
+
+//Function for Done
+  const handleDone = (id:number) => {
+    setTodos(todos.map((todo)=>todo.id===id?{...todo, isDone:!todo.isDone}:todo))
+  }
+
+//Function for Delete
+  const handleDelete = (id:number) => {
+    setTodos(todos.filter((todo)=>todo.id !== id));
+  };
+
+//Function for Edit
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo)=>(todo.id === id ? { ...todo, todo:editTodo}:todo
+        )));
+      setEdit(false);
+    
+  };
+//Edit focus using useRef hook
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  //sideEffect for Edit
+  useEffect(() =>{
+    inputRef.current?.focus();
+  }, [edit])
+
+  
+
   return (
-    <form className="todos_single">
-        <span className="todos_single--text">
-          {todo.todo}
+  <form className="todos_single" onSubmit={(e)=>handleEdit(e,todo.id)}>
+      {edit ? ( 
+          <input
+          ref={inputRef}
+           value={editTodo}
+           onChange={(e)=>setEditTodo(e.target.value)}
+           className="todos_single--text"/>
+        ) : todo.isDone ? (
+          <s className="todos_single--text">{todo.todo}</s>
+        ) : (
+          <span className="todos_single--text">{todo.todo}</span>
+        )}
 
           <div>
-            <span className="icon">
+            <span
+              className="icon"
+              onClick={()=>{
+                if(!edit && !todo.isDone){
+                setEdit(!edit);
+              }
+            }}
+            >
               <AiFillEdit/>
             </span>
-            <span className="icon">
+            <span className="icon" onClick={()=>handleDelete(todo.id)}>
             <AiFillDelete/>
             </span>
-            <span className="icon">
+            <span className="icon" onClick={()=>handleDone(todo.id)}>
               <MdDone/>
             </span>
           </div>
-        </span>
+      
     </form>
   )
 }
